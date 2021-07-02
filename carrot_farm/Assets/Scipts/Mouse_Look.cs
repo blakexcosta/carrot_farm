2f451b2c29d57156b48bb849eb153461d02d3b62
Blake Costa <blakecosta117@gmail.com>
2021-07-02 09:51:29 -0400

    Initialize project

---
+++ carrot_farm/Assets/Scipts/Mouse_Look.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse_Look : MonoBehaviour
+{
+    public float mouseSensitivity = 100f; //sensitivity level
+    public Transform playerBody; // player body object
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // lock cursor to center of screen
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // getting x and y input axis. multiplying by our mouse sensitivity and time.deltatime so framerate doesn't affect movement
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // rotate body of player on y axis based on sensitivity so can look left/right
+        playerBody.Rotate(Vector3.up * mouseX);
+        
+        //rotate bodyof player up and down around x axis
+        xRotation -= mouseY; //decreasing instead of increasing because otherwise it flips controls
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); //preventing over-rotating
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f); //keeping track of rotation and rotating object
+
+    }
+}
---
+++ carrot_farm/Assets/Scipts/Player_Movement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12; // spped
+    Vector3 velocity; //for gravity calculations
+    public float jumpHeight = 3f;
+    public float gravity = -9.81f;
+    public Transform groundCheck; // ground check object transform
+    public float groundDistance = 0.4f; //sphere distance ground should check
+    public LayerMask groundMask; // objects should check for
+    bool isGrounded; //private variable to check if grounded
+
+   
+    // jump velocity is v = sqrt(jumpheight-2*gravity)
+    // Update is called once per frame
+    void Update()
+    {
+        //project invisible sphere to prevent velocity build-up and see if object on ground
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if(isGrounded && velocity.y < 0) //resets velocity accordingly so long as object is grounded and 
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        //arrow in direction want to move
+        Vector3 move = transform.right * x + transform.forward * z; //direction want to move
+
+        //move object by speed and delta time to account for frame descrepancies
+        controller.Move(move * speed * Time.deltaTime);
+
+        //calculate jumping, mapped to spacebar
+        if(Input.GetButton("Jump") && isGrounded){
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        //gravity calculation
+        velocity.y += gravity * Time.deltaTime; //calculating gravity/velocity
+        controller.Move(velocity * Time.deltaTime); //multiply by deltatime again to get the squaring effect of delta Y = 1/2((g) * (t**2)) 
+    }
+}
